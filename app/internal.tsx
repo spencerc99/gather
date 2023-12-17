f2d72fc9d121ac59b882637d5ea406670f306743
Spencer Chang <spencerc99@gmail.com>
2023-12-16 23:54:00 -0800

    add back dev buttons in internal

--- app/internal.tsx
+++ app/internal.tsx
@@ -99,62 +99,62 @@ export default function ModalScreen() {
       <StyledButton disabled={isLoading} onPress={fetchBlocks}>
         Refresh Blocks
       </StyledButton>
+      <StyledParagraph>
+        Only do this if directed to do it in order to reset your schemas. It
+        will delete all your data.
+      </StyledParagraph>
+      <StyledButton
+        disabled={isLoading}
+        icon={isLoading ? <Spinner size="small" /> : null}
+        theme="red"
+        backgroundColor="$red8"
+        onPress={async () => {
+          setIsLoading(true);
+          try {
+            const results = await db.execAsync(
+              [
+                { sql: `DROP TABLE IF EXISTS collections;`, args: [] },
+                { sql: `DROP TABLE IF EXISTS blocks;`, args: [] },
+                { sql: `DROP TABLE IF EXISTS connections;`, args: [] },
+              ],
+              false
+            );
+
+            results
+              .filter((result) => "error" in result)
+              .forEach((result) => {
+                throw result;
+              });
+            // await initDatabases();
+          } catch (err) {
+            throw err;
+          } finally {
+            setIsLoading(false);
+          }
+        }}
+      >
+        Reset Databases
+      </StyledButton>
+      <XStack>
+        <StyledLabel bold>Token</StyledLabel>
+        <StyledParagraph ellipse>{arenaAccessToken}</StyledParagraph>
+      </XStack>
+      <StyledButton
+        onPress={() => {
+          AsyncStorage.setItem("seenIntro", "false");
+        }}
+      >
+        Reset intro seen
+      </StyledButton>
+      <StyledButton
+        onPress={() => {
+          AsyncStorage.removeItem(UserInfoId);
+        }}
+      >
+        Clear user
+      </StyledButton>
       {__DEV__ && (
         <YStack space="$1">
-          <StyledParagraph>
-            Only do this if directed to do it in order to reset your schemas. It
-            will delete all your data.
-          </StyledParagraph>
-          <StyledButton
-            disabled={isLoading}
-            icon={isLoading ? <Spinner size="small" /> : null}
-            theme="red"
-            backgroundColor="$red8"
-            onPress={async () => {
-              setIsLoading(true);
-              try {
-                const results = await db.execAsync(
-                  [
-                    { sql: `DROP TABLE IF EXISTS collections;`, args: [] },
-                    { sql: `DROP TABLE IF EXISTS blocks;`, args: [] },
-                    { sql: `DROP TABLE IF EXISTS connections;`, args: [] },
-                  ],
-                  false
-                );
-
-                results
-                  .filter((result) => "error" in result)
-                  .forEach((result) => {
-                    throw result;
-                  });
-                // await initDatabases();
-              } catch (err) {
-                throw err;
-              } finally {
-                setIsLoading(false);
-              }
-            }}
-          >
-            Reset Databases
-          </StyledButton>
-          <XStack>
-            <StyledLabel bold>Token</StyledLabel>
-            <StyledParagraph ellipse>{arenaAccessToken}</StyledParagraph>
-          </XStack>
-          <StyledButton
-            onPress={() => {
-              AsyncStorage.setItem("seenIntro", "false");
-            }}
-          >
-            Reset intro seen
-          </StyledButton>
-          <StyledButton
-            onPress={() => {
-              AsyncStorage.removeItem(UserInfoId);
-            }}
-          >
-            Clear user
-          </StyledButton>
           <H3>pending blocks</H3>
           <StyledParagraph>
             {JSON.stringify(pendingArenaBlocks, null, 2)}
