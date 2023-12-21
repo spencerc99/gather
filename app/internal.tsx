b879aba2cfa15d51386c6ee5aaff3040d851df60
Spencer Chang <spencerc99@gmail.com>
2023-12-21 12:42:21 -0600

    hide/show internal tools

--- app/internal.tsx
+++ app/internal.tsx
@@ -19,6 +19,7 @@ import {
 import { DatabaseContext } from "../utils/db";
 import {
   ButtonWithConfirm,
+  Icon,
   StyledButton,
   StyledLabel,
   StyledParagraph,
@@ -52,6 +53,7 @@ export default function ModalScreen() {
   const [selectedChannels, setSelectedChannels] = useState<ArenaChannelInfo[]>(
     []
   );
+  const [showInternalTools, setShowInternalTools] = useState(false);
   async function importSelectedChannels() {
     // TODO: this would ideally do it in the background asynchronously
     setIsLoading(true);
@@ -131,106 +133,122 @@ export default function ModalScreen() {
           ? `Importing ${selectedChannels.length} channels...`
           : `Import ${selectedChannels.length} channels`}
       </StyledButton>
-      <H3>Internal Developer Tools</H3>
-      <StyledText>
-        These are available for testing and debugging purposes. If you run into
-        any issues, please contact Spencer first, and he might direct you to
-        these buttons if there are issues :)
-      </StyledText>
-      <StyledButton
-        disabled={isLoading}
-        onPress={async () => {
-          setIsLoading(true);
-          try {
-            await trySyncPendingArenaBlocks();
-          } finally {
-            setIsLoading(false);
+      <H3>
+        Internal Dev Tools{" "}
+        <StyledButton
+          onPress={() => setShowInternalTools(!showInternalTools)}
+          circular
+          size="$2"
+          theme="white"
+          backgroundColor="$gray6"
+          icon={
+            <Icon name={showInternalTools ? "chevron-up" : "chevron-down"} />
           }
-        }}
-      >
-        <StyledParagraph>
-          Sync to Arena ({pendingArenaBlocks.length} pending)
-        </StyledParagraph>
-      </StyledButton>
-      <StyledButton
-        disabled={isLoading}
-        onPress={async () => {
-          setIsLoading(true);
-          try {
-            await trySyncNewArenaBlocks();
-          } finally {
-            setIsLoading(false);
-          }
-        }}
-      >
-        <StyledParagraph>Sync from Arena</StyledParagraph>
-      </StyledButton>
-      <StyledButton disabled={isLoading} onPress={fetchCollections}>
-        Refresh Collections
-      </StyledButton>
-      <StyledButton disabled={isLoading} onPress={fetchBlocks}>
-        Refresh Blocks
-      </StyledButton>
-      <StyledParagraph>
-        Only do this if directed to do it in order to reset your schemas. It
-        will delete all your data.
-      </StyledParagraph>
-      <StyledButton
-        disabled={isLoading}
-        icon={isLoading ? <Spinner size="small" /> : null}
-        theme="red"
-        backgroundColor="$red8"
-        onPress={async () => {
-          setIsLoading(true);
-          try {
-            const results = await db.execAsync(
-              [
-                { sql: `DROP TABLE IF EXISTS collections;`, args: [] },
-                { sql: `DROP TABLE IF EXISTS blocks;`, args: [] },
-                { sql: `DROP TABLE IF EXISTS connections;`, args: [] },
-              ],
-              false
-            );
-
-            results
-              .filter((result) => "error" in result)
-              .forEach((result) => {
-                throw result;
-              });
-            // await initDatabases();
-          } catch (err) {
-            throw err;
-          } finally {
-            setIsLoading(false);
-          }
-        }}
-      >
-        Reset Databases
-      </StyledButton>
-      <XStack>
-        <StyledLabel bold>Token</StyledLabel>
-        <StyledParagraph ellipse>{arenaAccessToken}</StyledParagraph>
-      </XStack>
-      <StyledButton
-        onPress={() => {
-          AsyncStorage.setItem("seenIntro", "false");
-        }}
-      >
-        Reset intro seen
-      </StyledButton>
-      <StyledButton
-        onPress={() => {
-          AsyncStorage.removeItem(UserInfoId);
-        }}
-      >
-        Clear user
-      </StyledButton>
-      {__DEV__ && (
-        <YStack space="$1">
-          <H3>pending blocks</H3>
+        ></StyledButton>
+      </H3>
+      {showInternalTools && (
+        <YStack space="$2">
+          <StyledText>
+            These are available for testing and debugging purposes. If you run
+            into any issues, please contact Spencer first, and he might direct
+            you to these buttons if there are issues :)
+          </StyledText>
+          <StyledButton
+            disabled={isLoading}
+            onPress={async () => {
+              setIsLoading(true);
+              try {
+                await trySyncPendingArenaBlocks();
+              } finally {
+                setIsLoading(false);
+              }
+            }}
+          >
+            <StyledParagraph>
+              Sync to Arena ({pendingArenaBlocks.length} pending)
+            </StyledParagraph>
+          </StyledButton>
+          <StyledButton
+            disabled={isLoading}
+            onPress={async () => {
+              setIsLoading(true);
+              try {
+                await trySyncNewArenaBlocks();
+              } finally {
+                setIsLoading(false);
+              }
+            }}
+          >
+            <StyledParagraph>Sync from Arena</StyledParagraph>
+          </StyledButton>
+          <StyledButton disabled={isLoading} onPress={fetchCollections}>
+            Refresh Collections
+          </StyledButton>
+          <StyledButton disabled={isLoading} onPress={fetchBlocks}>
+            Refresh Blocks
+          </StyledButton>
           <StyledParagraph>
-            {JSON.stringify(pendingArenaBlocks, null, 2)}
+            Only do this if directed to do it in order to reset your schemas. It
+            will delete all your data.
           </StyledParagraph>
+          <StyledButton
+            disabled={isLoading}
+            icon={isLoading ? <Spinner size="small" /> : null}
+            theme="red"
+            backgroundColor="$red8"
+            onPress={async () => {
+              setIsLoading(true);
+              try {
+                const results = await db.execAsync(
+                  [
+                    { sql: `DROP TABLE IF EXISTS collections;`, args: [] },
+                    { sql: `DROP TABLE IF EXISTS blocks;`, args: [] },
+                    { sql: `DROP TABLE IF EXISTS connections;`, args: [] },
+                  ],
+                  false
+                );
+
+                results
+                  .filter((result) => "error" in result)
+                  .forEach((result) => {
+                    throw result;
+                  });
+                // await initDatabases();
+              } catch (err) {
+                throw err;
+              } finally {
+                setIsLoading(false);
+              }
+            }}
+          >
+            Reset Databases
+          </StyledButton>
+          <XStack>
+            <StyledLabel bold>Token</StyledLabel>
+            <StyledParagraph ellipse>{arenaAccessToken}</StyledParagraph>
+          </XStack>
+          <StyledButton
+            onPress={() => {
+              AsyncStorage.setItem("seenIntro", "false");
+            }}
+          >
+            Reset intro seen
+          </StyledButton>
+          <StyledButton
+            onPress={() => {
+              AsyncStorage.removeItem(UserInfoId);
+            }}
+          >
+            Clear user
+          </StyledButton>
+          {__DEV__ && (
+            <YStack space="$1">
+              <H3>pending blocks</H3>
+              <StyledParagraph>
+                {JSON.stringify(pendingArenaBlocks, null, 2)}
+              </StyledParagraph>
+            </YStack>
+          )}
         </YStack>
       )}
       {/* TODO: bring this back when working */}
