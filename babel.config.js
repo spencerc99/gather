4ea2963c59786b6d62b0a62dbc9551809441139f
Spencer Chang <spencerc99@gmail.com>
2024-04-26 10:26:32 -0700

    wip fix dependencies

--- babel.config.js
+++ babel.config.js
@@ -3,8 +3,6 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo"],
     plugins: [
-      // Required for expo-router
-      "expo-router/babel",
       // Required for react-native-reanimated, must be listed last
       "react-native-reanimated/plugin",
     ],
