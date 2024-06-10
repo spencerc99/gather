80f22d953ca9d2ccc3e232ff40adec32942f0df3
Spencer Chang <spencerc99@gmail.com>
2024-06-09 18:14:50 -0700

    fix length bug

--- utils/db.tsx
+++ utils/db.tsx
@@ -599,67 +599,6 @@ export function DatabaseProvider({ children }: PropsWithChildren<{}>) {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["blocks"] });
     },
-    onMutate: async ({ blocksToInsert, collectionId }) => {
-      // Cancel any outgoing refetches
-      // (so they don't overwrite our optimistic update)
-      await queryClient.cancelQueries({
-        queryKey: ["blocks", { collectionId }],
-      });
-
-      // Snapshot the previous value
-      const previousBlocks = queryClient.getQueryData([
-        "blocks",
-        { collectionId },
-      ]);
-
-      // Optimistically update to the new value
-      queryClient.setQueryData<InfiniteData<Block[]>>(
-        ["blocks", { collectionId }],
-        // @ts-ignore
-        (old) => {
-          const optimisticBlocks = blocksToInsert.map((block) => ({
-            id: "...",
-            ...block,
-            collectionIds: collectionId ? [collectionId] : [],
-            createdAt: new Date(),
-            updatedAt: new Date(),
-            numConnections: collectionId ? 1 : 0,
-            remoteConnectedAt: block.remoteConnectedAt
-              ? new Date(block.remoteConnectedAt)
-              : undefined,
-          }));
-          if (!old) {
-            return optimisticBlocks;
-          }
-          if ("pages" in old) {
-            // format {"pageParams": [0], "pages": [{"blocks": [Array], "nextId": 1, "previousId": undefined}]}
-            return {
-              ...old,
-              pages: old.pages.map((page, idx) =>
-                idx > 0
-                  ? page
-                  : {
-                      // @ts-ignore
-                      blocks: [...optimisticBlocks, page.blocks],
-                      ...page,
-                    }
-              ),
-            };
-          }
-
-          return [...(old || []), ...optimisticBlocks];
-        }
-      );
-
-      // Return a context object with the snapshotted value
-      return { previousBlocks };
-    },
-    onError: (_err, { collectionId }, context) => {
-      queryClient.setQueryData(
-        ["blocks", { collectionId }],
-        context?.previousBlocks
-      );
-    },
   });
   const createBlocks = createBlocksMutation.mutateAsync;
 
